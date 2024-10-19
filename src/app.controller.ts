00dba2448fe148bc4abd6731f2f3c5eecc4669e0
NopeThisIsShini <getin2santa@gmail.com>
2024-10-19 10:57:23 +0530

    understand routes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,44 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Patch, Post, Req } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Request } from 'express';
 
-@Controller()
+@Controller('/user')
 export class AppController {
   constructor(private readonly appService: AppService) {}
-  // define the route under controller 
-  @Get('/hello')
+  // define the route under controller
+  // if i here give any route so everytime for all child route i need to define base route, so avoid this i give it in controller 
+  
+  // defining the base route
+  @Get('')
   getHello(): string {
     return this.appService.getHello();
+  }
+  //now this means /user/santa
+  @Get('/santa')
+  getPass(): string {
+    return 'Hello shinigami!';
+  }
+
+  // post 
+  @Post('')
+  store(@Req() req: Request) {
+    console.log(req.body)
+    return 'req';
+  }
+
+  // params pass in get api
+  @Get('/:userId')
+  getUserById(@Param() userId: number) {
+    return userId;
+  }
+
+  @Patch('/:userId')
+  update(@Req() req: Request) {
+    return req.body
+  }
+  // delete route 
+  @Delete('/:userId')
+  deleteUser(@Param() params: { userId: number }) {
+    return params; 
   }
 }
